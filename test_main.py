d9a6affbbbf7d0b5efe768d45c8a2aad032411c7
Alex Pineda <h0h04dt@homeoffice.wal-mart.com>
2021-09-17 18:02:37 -0500

    reporte

--- test_main.py
+++ test_main.py
@@ -1,5 +1,12 @@
+import pytest
+
 from main import suma
 
 
 def test_suma_correcta():
     assert suma(1, 1) == 2
+
+
+def test_suma_exception():
+    with pytest.raises(TypeError):
+        suma(21, 21)
